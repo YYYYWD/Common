d710ef21644c5918a1461e34c3ff729c4f1d92bf
YYYYWD <278580633@qq.com>
2018-11-27 21:22:43 +0800

    update

--- code/Leetcode/未命名1.cpp
+++ code/Leetcode/未命名1.cpp
@@ -1,28 +1,75 @@
 #include<iostream>
 using namespace std;
 
-class Test{
+//菱形继承
+class A
+{
 public:
-    Test(){};
-    Test (int x){ /*int_x = x;*/};
-    //void show(){cout<< int_x << endl;}
-
+      int _a;
+      A(){
+      	cout<<"this A()"<<endl;
+	  }
+	  ~A(){
+      	cout<<"this ~A()"<<endl;
+	  }
+	  A(const A&aaa){
+	  	cout<<"this copy A()"<<endl;
+	  }
+	  
 };
-int a=8; 
-class Mytest:public Test{
+class B
+{
 public:
-    Mytest():t(a) {
-      //Test(112);            //  构造函数只能在初始化列表中被显示调用，不能在构造函数内部被显示调用
-    	
-	};
-    int &t;
-    
+      int _b;
+      B()
+      
+      	{
+      	cout<<"this B()"<<endl;
+	  }
+	  ~B()
+      
+      	{
+      	cout<<"this ~B()"<<endl;
+	  }
+	  
 };
-
+class C 
+{
+public:
+      int _c;
+      C()
+      {
+      	cout<<"this C()"<<endl;
+	  }
+	   ~C()
+      {
+      	cout<<"this ~C()"<<endl;
+	  }
+};
+class D :public B, public C
+{
+public:
+      int _d;
+      A aa;
+      D(A a1,int dd):_d(dd)
+	  {
+	  	aa(a1)
+	  	cout<<"this D()"<<endl;
+	  		cout<<"------"<<endl;
+	  };
+	   ~D()
+      {
+      	cout<<"this ~D()"<<endl;
+	  }
+};
 int main()
 {
- Test *p = new Mytest();
- //p->show();
- return 0;
+      
+      A a;
+      D d(a,5);
+    	cout<<"-----"<<endl;
+     
+	
+      return 0;
 }
 
